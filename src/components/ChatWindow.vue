ccc0fd1f47e9c7dca02d21c56f6563ba0cfd08c0
MatheusTomazella <matheustomazella250504@gmail.com>
2020-08-13 20:24:53 -0300

    2020-08-13 - Adicinando Vue

---
+++ src/components/ChatWindow.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="bordinha" id="maxChat">
+        <div class="botoes">
+            <i class="fa fa-window-minimize icon" aria-hidden="true" onclick=" hide() "></i>
+            <i class="fas fa-window-maximize icon"></i>
+            <i class="fa fa-times icon" aria-hidden="true"></i>
+        </div>
+        <div class="col-12 chat" id="chat"></div> 
+        <div class="col-12 write">
+            <form class="write row" autocomplete="off">
+                <input autocomplete="false" name="hidden" type="text" style="display:none;">
+                <input type="text" class="write badge-pill px-3" autocapitalize="on" autocomplete="off" name="input" id="input">
+                <button type="button" class="send rounded rounded-circle" name="send" onclick="post()"> <i class="fa fa-location-arrow icon send" aria-hidden="true"></i> </button>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ChatWindow'
+}
+</script>
---
+++ src/components/Connection.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="connection">
+        <div class="conxIcon">
+            <i class="fa fa-user-circle conxIcon" onclick="conxUI()" aria-hidden="true"> </i>
+            <label class="conxIcon" id="conxIconLabel"> Conexão </label>
+        </div>
+        <div class=" conxBox " id="conx">
+            <div class="conx">
+                <input type="text" id="userCode" class="cod" value="1" placeholder="Código">
+                <select name="chatType" class="cod" id="chatType">
+                    <option value="user" selected>User</option>
+                    <option value="admin">Admin</option>
+                </select>
+                <button class="conxBtn cod" onclick="connect()">Conectar</button>
+            </div>
+
+            <div class=" userInfo conx " id="userInfo" style="display: none;">
+                <label class="userInfo" id="displayName">   </label>
+                <label class="userInfo" id="name">          </label>
+                <label class="userInfo" id="email">         </label>
+                <label class="userInfo" id="id">            </label>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Connection',
+}
+</script>
